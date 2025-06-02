d16a75fc76f6ceca6b12bba1fdc2bdab033f6d3f
vamshi-krishna-vijayagiri <vamshi.vijayagiri@gmail.com>
2025-06-02 23:59:56 +0530

    db connection change

--- Server.js
+++ Server.js
@@ -12,11 +12,23 @@ const port = 5000;
 
 // Create MySQL connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Vamshi@mysql',
-  database: 'nxtwavedb',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
+
+
+connection.connect((err) => {
+  if (err) {
+    console.error('❌ Database connection failed:', err);
+    return;
+  }
+  console.log('✅ Connected to MySQL database');
+});
+
+
 
 // Connect to MySQL
 db.connect((err) => {
